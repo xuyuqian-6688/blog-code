2ebdb26d8f437eb18f3f338d373acf53fd86e771
xuyuqian-6688 <62925507+xuyuqian-6688@users.noreply.github.com>
2023-01-09 11:08:34 +0800

    Update config.js
    1

--- bugfix脚本/config.js
+++ bugfix脚本/config.js
@@ -9,9 +9,9 @@ module.exports = {
     SendKey:'you sendkey',
     users: [
         {
-            aid: "you aid",
-            uuid:"you uuid",
-            cookie: `you cookie`,//使用字符串模板，防止cookie里有引号导致标签闭合问题
+            aid: "2608",
+            uuid:"7028194528561726976",
+            cookie: `_ga=GA1.2.1643124801.1660030957; __tea_cookie_tokens_2608=%257B%2522user_unique_id%2522%253A%25227028194528561726976%2522%252C%2522web_id%2522%253A%25227028194528561726976%2522%252C%2522timestamp%2522%253A1651743305674%257D; MONITOR_WEB_ID=ba4dfafa-3a8a-44e7-b165-8d11bca7cab3; odin_tt=3d2e6f0e76e1d8510a39e7437242966ed9dae79a49481dda08f7c6ad378ea76eba5a748d855771b6a6bb42507f8be29a372927d7668e1c87eba51b1d0bd00f47; n_mh=LC3yUz29L4FsGFPrQ36p5PNk2MTKUB_Ddbd1Z88L5xA; sid_guard=51d91731d73ba68be73469154367ea0c%7C1664357720%7C31536000%7CThu%2C+28-Sep-2023+09%3A35%3A20+GMT; uid_tt=b281887388916693572a01fef79f1d5e; uid_tt_ss=b281887388916693572a01fef79f1d5e; sid_tt=51d91731d73ba68be73469154367ea0c; sessionid=51d91731d73ba68be73469154367ea0c; sessionid_ss=51d91731d73ba68be73469154367ea0c; sid_ucp_v1=1.0.0-KGVhNTNiNWM4ZjExZjNjODE4ZDdhY2VkMjUwMmJiYzJiMWQ1Njc3OGYKFwiuxMCKtIzTARDYqtCZBhiwFDgCQOwHGgJsZiIgNTFkOTE3MzFkNzNiYTY4YmU3MzQ2OTE1NDM2N2VhMGM; ssid_ucp_v1=1.0.0-KGVhNTNiNWM4ZjExZjNjODE4ZDdhY2VkMjUwMmJiYzJiMWQ1Njc3OGYKFwiuxMCKtIzTARDYqtCZBhiwFDgCQOwHGgJsZiIgNTFkOTE3MzFkNzNiYTY4YmU3MzQ2OTE1NDM2N2VhMGM; _gid=GA1.2.1262772281.1673226419; _tea_utm_cache_2608={%22utm_source%22:%22daohang%22}`,//使用字符串模板，防止cookie里有引号导致标签闭合问题
         }
     ]
 
